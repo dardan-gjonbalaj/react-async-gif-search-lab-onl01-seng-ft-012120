3bc0d11278cb459f7253880b7392ae7f33ba2534
xdundun <xdundun@gmail.com>
2020-06-29 14:27:36 -0400

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default class GifSearch extends React.Component {
+  state = {
+    search: "",
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.fetchImages(this.state.search);
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.search}
+            onChange={(event) => this.setState({ search: event.target.value })}
+          />
+        </form>
+      </div>
+    );
+  }
+}
